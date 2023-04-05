77709553f6361a9ac123dfc0a743fef4143eba2e
Joshua Ow <joshuaow@gmail.com>
2023-04-04 21:14:01 -0500

    tailwind works

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from 'vite-plugin-tailwind'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwind()
-  ]
+  plugins: [react()],
 })
